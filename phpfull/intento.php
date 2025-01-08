b1c21dec6d11abd59b0f81970cc2383f2f8b6579
Vicrator <pavlo23deenero@gmail.com>
2025-01-08 09:11:42 -0600

    Actualización del proyecto

---
+++ phpfull/intento.php
@@ -0,0 +1,10 @@
+<?php
+// URL de redirección
+$url = "https://www.youtube.com/watch?v=vdp-kyvP_kw&ab_channel=AQUISI";
+
+// Realiza la redirección
+header("Location: $url");
+
+// Asegúrate de detener la ejecución del script después de la redirección
+exit();
+?>
