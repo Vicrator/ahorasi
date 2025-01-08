2c3bdcce1411a6f455d2f717e92571e79fda99fe
Vicrator <pavlo23deenero@gmail.com>
2025-01-08 09:53:33 -0600

    Actualización del proyecto

--- phpfull/conexion.php
+++ phpfull/conexion.php
@@ -1,8 +1,8 @@
 
 <?php
 /*Puedo ocupar php.init para evitar que los errores se vean en produccion*/ 
+ini_set('display_errors', 'On');
+ini_set('error_reporting', E_ALL);
 $conexion= new mysqli("basegym.mysql.database.azure.com","victorestudiante","soyelestudiante23#","gymfitpass23_past");
 $conexion->set_charset("utf8");
-
-
 ?>
