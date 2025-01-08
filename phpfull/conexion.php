dc83533edfc69fcd894e45487d16162dc36fb252
Vicrator <pavlo23deenero@gmail.com>
2025-01-07 21:45:50 -0600

    Actualización del proyecto

--- phpfull/conexion.php
+++ phpfull/conexion.php
@@ -1,8 +1,8 @@
 
 <?php
 /*Puedo ocupar php.init para evitar que los errores se vean en produccion*/ 
-$conexion= new mysqli("basegym.mysql.database.azure.com","victorestudiante","soyelestudiante23#","gymfitpass23_past",MYSQLI_CLIENT_SSL );
-$conexion->set_charset("utf8");
+//$conexion= new mysqli("basegym.mysql.database.azure.com","victorestudiante","soyelestudiante23#","gymfitpass23_past",MYSQLI_CLIENT_SSL );
+//$conexion->set_charset("utf8");
 
 
 ?>
