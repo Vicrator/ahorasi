fa7786a94ce3c61a7d052d9c40eede5e58acc10d
Vicrator <pavlo23deenero@gmail.com>
2025-01-08 09:37:16 -0600

    Actualización del proyecto

--- phpfull/login.php
+++ phpfull/login.php
@@ -1,43 +1,45 @@
 <?php
 include("conexion.php");
 if (!empty($_POST["btnsesion"])) {
-    if (empty($_POST["usuario"]) || empty($_POST["password"])) {        
+    if (empty($_POST["usuario"]) || empty($_POST["password"])) {
         //echo '<div class="alert alert-danger text-center" >User o password vacios</div>'; 
-        
-    }
-    else{
-        $usuario=$_POST["usuario"];
-        $password=$_POST["password"];
-        $sql =$conexion -> prepare("SELECT * FROM usuarios WHERE Usuario = ? and contrasena = ? ");
-        $sql->bind_param("ss",$usuario,$password );
+
+    } else {
+        $usuario = $_POST["usuario"];
+        $password = $_POST["password"];
+        $sql = $conexion->prepare("SELECT * FROM usuarios WHERE Usuario = ? and contrasena = ? ");
+        $sql->bind_param("ss", $usuario, $password);
         $sql->execute();
-        
-        $Resultados=$sql->get_result();
+
+        $Resultados = $sql->get_result();
         if ($Resultados->num_rows > 0) {
             /*Mientras creo la parte del usuario*/
             //echo '<div class="alert alert-danger text-center">Muy bien</div>'; 
-            $comparacion= $Resultados->fetch_assoc();
-            if($comparacion['tipousuario'] == "1"){
+            $comparacion = $Resultados->fetch_assoc();
+            if ($comparacion['tipousuario'] == "1") {
                 //session_start();
                 //$_SESSION["Usuario"]=$comparacion['Usuario'];
                 //echo '<div class="alert alert-danger text-center">Entro</div>'; 
                 /*Tipo de vista usuario tipo cliente
-                echo '<div class="alert alert-danger text-center">Tipo de usuario cliente</div>'; */ 
-                header("location:https://gymfitpass-f5c7hrcyd7dkcxh2.canadacentral-01.azurewebsites.net");
-                exit;
-            }
-            else{
-                /*Tipo de vista usuario tipo gimnasio
-                echo '<div class="alert alert-danger text-center">Tipo de usuario gimnasio</div>'; */ 
-            }
-    
+                echo '<div class="alert alert-danger text-center">Tipo de usuario cliente</div>'; */
+                //header("location:https://gymfitpass-f5c7hrcyd7dkcxh2.canadacentral-01.azurewebsites.net");
+                //exit;
 
-        }else{
+
+                // URL de redirección
+                $url = "https://www.youtube.com/watch?v=vdp-kyvP_kw&ab_channel=AQUISI";
+
+                // Realiza la redirección
+                header("Location: $url");
+
+                // Asegúrate de detener la ejecución del script después de la redirección
+                exit();
+            } else {
+                /*Tipo de vista usuario tipo gimnasio
+                echo '<div class="alert alert-danger text-center">Tipo de usuario gimnasio</div>'; */
+            }
+        } else {
             //echo '<div class="alert alert-danger text-center">Usuario no existe</div>'; 
         }
-
-
-       }
-    
+    }
 }
-?>
