a20c903f6486600423d2dfc8480529a4f4b645b1
Vicrator <pavlo23deenero@gmail.com>
2025-01-07 20:22:07 -0600

    Actualización del proyecto

--- phpfull/conexion.php
+++ phpfull/conexion.php
@@ -3,10 +3,14 @@
 /*Puedo ocupar php.init para evitar que los errores se vean en produccion*/ 
 $conexion= new mysqli("basegym.mysql.database.azure.com","victorestudiante","soyelestudiante23.","Gymfitpass23-past");
 $conexion->set_charset("utf8");
-if ($conexion) {
-    echo "Bien";
-}
-else{
-    echo "mal";
+
+
+// Verificar si la conexión fue exitosa
+if ($conexion->connect_error) {
+    // Log de errores en lugar de mostrar mensajes en pantalla
+    error_log("Error de conexión: " . $conexion->connect_error);
+    die("Error al conectar con la base de datos. Inténtalo más tarde.");
+} else {
+    echo "Conexión exitosa.";
 }
 ?>
