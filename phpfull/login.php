47710f133912470f7cd280fd04b192897b73d16e
Vicrator <pavlo23deenero@gmail.com>
2025-01-11 22:07:20 -0600

    Actualización del proyecto

--- phpfull/login.php
+++ phpfull/login.php
@@ -37,8 +37,8 @@ if (!empty($_POST["btnsesion"])) {
                 echo '<div class="alert alert-danger text-center">Tipo de usuario gimnasio</div>'; */
             }
         } else {
-            $mensaje= '<div class="alert alert-danger text-center">Usuario no existe</div>'; 
-            header("Location:/iniciousuario.php");
+            
+            header("Location:/iniciousuario.php?mensaje=<div class='alert alert-danger text-center'>Usuario no existe</div>");
             exit();
         }
     }
