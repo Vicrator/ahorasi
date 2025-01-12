83079738e7bd7e0ab17153801a856a8f9d09d2eb
Vicrator <pavlo23deenero@gmail.com>
2025-01-12 16:28:00 -0600

    Actualización del proyecto

--- phpfull/registrarmembresia.php
+++ phpfull/registrarmembresia.php
@@ -1,27 +1,35 @@
 <?php
 include("conexion.php");
 session_start();
-if (!empty($_POST["btnregistrar"])) {
-
-
-
-// Validar si se obtuvo el id_gimnasio de $_GET
-if (isset($_SESSION["Usgimnasio"]["id"])) {
-    die("No se proporcionó el id_gimnasio en la URL.");
+if (empty($_POST["nombre_membresia"]) || empty($_POST["precio"]) || empty($_POST["descripcion"])) {
+    header("Location:/membresiadetalles.php?mensaje=<div class='alert alert-danger text-center'>No se llenaron todos los datos</div>");
+    exit();
 } else {
-    $id_gimnasio = $_SESSION["Usgimnasio"]["id"];
 
-    // Verificar si se envió el formulario
 
-    $nombre_membresia =$_POST['nombre_membresia'];
-    $precio =$_POST['precio'];
-    $descripcion = $_POST['descripcion'];
-    $activo =$_POST['activo'];
+    if (!empty($_POST["btnregistrar"])) {
 
-    // Insertar en la base de datos
-    $sql = "INSERT INTO membresias (id_gimnasio, Nombre_membresia, precio, descripcion, Activo) VALUES ('$id_gimnasio', '$nombre_membresia', '$precio', '$descripcion', '$activo')";
 
+
+        // Validar si se obtuvo el id_gimnasio de $_GET
+        if (isset($_SESSION["Usgimnasio"]["id"])) {
+            ("No se proporcionó el id_gimnasio en la URL.");
+        } else {
+            $id_gimnasio = $_SESSION["Usgimnasio"]["id"];
+
+            // Verificar si se envió el formulario
+
+            $nombre_membresia = $_POST['nombre_membresia'];
+            $precio = $_POST['precio'];
+            $descripcion = $_POST['descripcion'];
+            $activo = isset($_POST['activo']) ? 1 : 0;
+
+            // Insertar en la base de datos
+            $sql = "INSERT INTO membresias (id_gimnasio, Nombre_membresia, precio, descripcion, Activo) VALUES ('$id_gimnasio', '$nombre_membresia', '$precio', '$descripcion', '$activo')";
+            $stms=$conexion->prepare($sql);
+            $stms->execute();
+            header("Location:/membresiadetalles.php?mensaje=<div class='alert alert-danger text-center'>Datos insertados correctamente</div>");
+    exit();
+        }
+    }
 }
-
-}
-?>
