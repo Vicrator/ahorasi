da5fab03a5c2903a3ae900631f143db555600715
Vicrator <pavlo23deenero@gmail.com>
2025-01-12 20:42:35 -0600

    Actualización del proyecto

--- phpfull/eliminarmembresia.php
+++ phpfull/eliminarmembresia.php
@@ -6,15 +6,9 @@ if (isset($_GET['id_membresia'])) {
     $id_membresia = intval($_GET['id_membresia']); // Asegura que sea un entero
     $sql = $conexion->prepare("UPDATE membresias SET activo = 0 WHERE id = ?");
     $sql->bind_param("i", $id_membresia);
-
-    if ($sql->execute()) {
+    $sql->execute();
         // Redirige con un mensaje de éxito
         header("Location: /catalogo.php?mensaje=Membresia inabilitada");
         exit();
-    } else {
-        // En caso de error
-        header("Location: /catalogo.php?mensaje=error");
-        exit();
-    }
-}
+    } 
 ?>
